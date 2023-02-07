79540614ee3332075bdb7a4a68e005e9fd526166
Elizaveta Bystrova <114299473+nocturnodechile@users.noreply.github.com>
2023-02-07 17:35:05 +0300

    Add files via upload

---
+++ slider/sliderDomManipulations.js
@@ -0,0 +1,31 @@
+
+
+const leftBtn = document.querySelector('#left');
+const rightBtn = document.querySelector('#right');
+const items = document.querySelector('#items');
+const computedStyles = getComputedStyle(items);
+
+let currentRight = 0;
+
+rightBtn.addEventListener('click', e => {
+  e.preventDefault();
+  let currentRight = parseInt (computedStyles.right);
+
+  if (currentRight < 500) {
+
+    items.style.right = `${currentRight + 100}px`;
+
+  }
+
+});
+
+leftBtn.addEventListener('click', e => {
+  e.preventDefault();
+  let currentRight = parseInt (computedStyles.right);
+  
+  if (currentRight > 0) {
+
+    items.style.right = `${currentRight - 100}px`;
+
+  }
+});
